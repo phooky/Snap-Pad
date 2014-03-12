81a01563dcd0a868c5f25e27aacefbe4c188fcad
Adam Mayer <me@phooky.name>
2014-03-12 17:59:26 -0400

    added more flexibility to rand read script

--- code/randread.py
+++ code/randread.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python
 import serial
 import sys
+import argparse
 
-p = serial.Serial('/dev/ttyACM0',115200)
+p = None
 #p = sys.stdout
 
 def read():
+    global p
     c = 'P\n\r'
     p.write(c)
     p.flush()
     return p.read(512)
 
 def dump(l):
-    while l > 255 or l < 0:
+    while l > 512 or l < 0:
         sys.stdout.write(read())
-        l = l - 255
-    sys.stdout.write(read())
+        l = l - 512
+    if l > 0:
+        sys.stdout.write(read()[0:l])
 
 if __name__ == '__main__':
-    dump(-1)
+    parser = argparse.ArgumentParser(description='Pull random numbers from the snap-pad.')
+    parser.add_argument('-c', '--count', type=int, default=-1)
+    parser.add_argument('-P', '--port', default='/dev/ttyACM0')
+    args = parser.parse_args()
+    p = serial.Serial(args.port,115200)
+    dump(args.count)
              
