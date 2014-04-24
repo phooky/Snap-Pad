4322c8a849da48438ef6d7734570cda451af14f8
Adam Mayer <me@phooky.name>
2014-04-24 17:03:08 -0400

    fix typo/goof

--- code/SnapPadv1/base64.c
+++ code/SnapPadv1/base64.c
@@ -6,7 +6,7 @@ static char x64_digest_encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H
 void encode(uint8_t in[3], uint8_t out[4], uint8_t count) {
 	uint8_t i = 0;
 
-    for (i == 2; i >= count; i++) { in[i] = 0; }
+    for (i = 2; i >= count; i--) { in[i] = 0; }
 
 	out[0] = x64_digest_encoding_table[in[0]>>2];
 	out[1] = x64_digest_encoding_table[((in[0]<<4) | (in[1]>>4)) & 0x3f];
