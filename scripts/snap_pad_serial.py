b12fdfcde6a7d59f0b0721a3425cf11c767940e6
Adam Mayer <me@phooky.name>
2014-05-05 19:15:04 -0400

    serial snap pad identification

--- scripts/snap_pad_serial.py
+++ scripts/snap_pad_serial.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python
-import usb.core
+import serial
+import serial.tools.list_ports
 
 vendor_id=0x2047
 product_id=0x03ee
 
-def find_pad(sn=None):
-    """
-    Return a usb device representing a Snap-Pad with the given serial 
-    number. If no serial number is provided and only one Snap-Pad is
-    present, use that Snap-Pad.
-    """
-    pads = usb.core.find(idVendor=vendor_id,idProduct=product_id,find_all=True)
-    if not pads:
-        raise RuntimeError("No Snap-Pads found")
-    if sn:
-        for p in pads:
-            if p.serial_number() == sn:
-                return p
-        raise RuntimeError("No Snap-Pad with serial # {0} found".format(sn))
-    else:
-        if len(pads) > 1:
-            raise RuntimeError("Multiple Snap-Pads found; specify serial #")
-        return pads[0]
-
+def find_snap_pads():
+    return [SnapPad(x['port'],x['iSerial']) for x in serial.tools.list_ports.list_ports_by_vid_pid(vendor_id,product_id)]
+        
 class SnapPad:
-    def __init__(self,sn=None):
-        self.d = find_pad(sn)
-        print "Found pad with SN {0}".format(self.d.serial_number())
+    def __init__(self,port,sn):
+        self.p = serial.Serial(port)
+        self.sn = sn
+        print "Found pad with SN {0}".format(self.sn)
+
+print find_snap_pads()
 
             
             
