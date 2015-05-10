319acc210c3b65d3b0819460e5dbfc95d7584a8c
Adam Mayer <phooky@gmail.com>
2015-05-10 10:18:23 -0400

    adding module stuff

---
+++ python/snap_pad/__init__.py
@@ -0,0 +1,2 @@
+from .snap_pad import SnapPad, list_snap_pads, add_pad_arguments, find_our_pad
+
---
+++ python/snap_pad/util.py
@@ -0,0 +1,4 @@
+
+def sign(msg, salt, pad):
+    'Sign the given message with the provided salt and pad bits.'
+
