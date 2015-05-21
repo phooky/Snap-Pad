20ba54d52aacf594677e444d28813f0d6532b333
Adam Mayer <phooky@gmail.com>
2015-05-21 14:15:40 -0400

    creating more verbose and complete configuration file

--- code/SnapPadv1/config.h
+++ code/SnapPadv1/config.h
@@ -8,14 +8,22 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-// Builds:
-// Default -- release build
-// DEBUG -- debugging build
-// FACTORY_TEST -- factory test build
+/** This configuration file builds the default release firmware by default.
+	If you want to build the debug or factory test variants, define one
+	of the following macros:
+	DEBUG -- debugging build
+	FACTORY_TEST -- factory test build
+	*/
 
+/** Define the major and minor versions of this firmware. The version is
+	written as "M.mV", where M is the major version number, m is the
+	minor version number, and V is the (optional) one character long
+	build variant type. */
 #define MAJOR_VERSION 1
 #define MINOR_VERSION 1
 
+/** The firmware variant is defined based on the values of the
+	DEBUG and FACTORY_TEST flags. */
 #if defined(DEBUG)
 #define VARIANT "D"
 #elif defined(FACTORY_TEST)
@@ -23,6 +31,10 @@
 #else
 #define VARIANT ""
 #endif
+
+/** Define exactly one of options below to indicate which NAND
+	chip the target board is using. */
+#define NAND_CHIP_S34ML01G2			// 2Gb Samsung SLC flash
 
 enum {
 	CS_INDETERMINATE = 0,
