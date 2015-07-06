357d056c947b759856e1bfdb6273a7e2aaff4d04
Adam Mayer <phooky@gmail.com>
2015-07-06 14:59:04 -0400

    added mismatch tests and added ascii to round test

--- python/snap_pad/test/test_containers.py
+++ python/snap_pad/test/test_containers.py
@@ -42,13 +42,34 @@ class EncryptedMessageTest(unittest.TestCase):
         e2 = EncryptedMessage.from_binary(b)
         self.assertEqual(e2, self.enc)
 
+    def testMismatches(self):
+        j = EncryptedMessage.to_json(self.enc)
+        b = EncryptedMessage.to_binary(self.enc)
+        a = EncryptedMessage.to_ascii(self.enc)
+        with self.assertRaises(Exception) as context:
+            EncryptedMessage.from_json(b)
+        with self.assertRaises(Exception) as context:
+            EncryptedMessage.from_json(a)
+        with self.assertRaises(Exception) as context:
+            EncryptedMessage.from_binary(j)
+        with self.assertRaises(Exception) as context:
+            EncryptedMessage.from_binary(a)
+        with self.assertRaises(Exception) as context:
+            EncryptedMessage.from_ascii(j)
+        with self.assertRaises(Exception) as context:
+            EncryptedMessage.from_ascii(b)
+
+
     def testRound(self):
         a = EncryptedMessage.to_json(self.enc)
         e2 = EncryptedMessage.from_json(a)
         self.assertEqual(self.enc, e2)
         b = EncryptedMessage.to_binary(e2)
         e3 = EncryptedMessage.from_binary(b)
-        self.assertEqual(e3, self.enc)
+        self.assertEqual(e3, e2)
+        c = EncryptedMessage.to_ascii(e2)
+        e4 = EncryptedMessage.from_ascii(c)
+        self.assertEqual(e4, self.enc)
 
     def makeTestMsg(self,len):
         return b''.join([chr(random.randint(0x20,0x5a)) for i in range(len)])
