afb3d30c3791f24fcfec166e09c20aa5439f4e7d
Adam Mayer <phooky@gmail.com>
2015-07-23 19:35:43 -0400

    turns out that darwin works the same way

--- python/snap_pad/serial_util.py
+++ python/snap_pad/serial_util.py
@@ -6,7 +6,7 @@ from snap_pad_vid_pid import vendor_id, product_id
 
 linux_port_re = re.compile('VID:PID={0:04x}:{1:04x} SNR=([0-9A-Fa-f]+)'.format(vendor_id,product_id))
 
-def make_candidate_linux(c):
+def make_candidate_linux_or_mac(c):
 	'Turn a comport entry into a (portname,serial#) tuple'
 	m = linux_port_re.search(c[2])
 	if m:
@@ -16,13 +16,11 @@ def make_candidate_linux(c):
 
 
 def find_snap_pads():
-	if sys.platform.startswith('linux'):
-		return filter(None,map(make_candidate_linux,serial.tools.list_ports.comports()))
-	elif sys.platform.startswith('darwin'):
-		pass
+	if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
+		return filter(None,map(make_candidate_linux_or_mac,serial.tools.list_ports.comports()))
 	elif sys.platform.startswith('win32'):
 		pass
-	elif syst.platform.startswith('cygwin'):
+	elif sys.platform.startswith('cygwin'):
 		pass
 
 
